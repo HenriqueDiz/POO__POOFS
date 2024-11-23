e77e61946f03ee28d4bbb5afda849d90402f79bc
Tomás Gonçalves <166638083+bauertommy4@users.noreply.github.com>
2024-11-23 19:53:43 +0000

    coisinha no final

--- ProdutoFarmaciaSemDescricao.java
+++ ProdutoFarmaciaSemDescricao.java
@@ -23,9 +23,4 @@ class ProdutoFarmaciaSemDescricao extends Produto{
         }
         return taxaBase / 100;
     }
-<<<<<<< HEAD
 }
-=======
-
-}
->>>>>>> 79927db3150e896b2476a586ace5c8b98a893321
